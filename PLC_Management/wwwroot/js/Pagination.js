d32e24936a60d4fc29e27a39693e0cf57b9fdbe0
xuanco941 <xuanco941@gmail.com>
2022-05-03 17:02:12 +0700

    pagination js

---
+++ PLC_Management/wwwroot/js/Pagination.js
@@ -0,0 +1,14 @@
+﻿var url_string = window.location.href;
+var url = new URL(url_string);
+var page = url.searchParams.get("page");
+
+window.addEventListener('load', () => {
+    let pageTtems = Array.from(document.querySelectorAll('.page-item'));
+
+    pageTtems.forEach((item) => {
+        if (parseInt(item.children[0].textContent) == parseInt(page) || !page) {
+            item.classList.add('active');
+        }
+    })
+
+})
